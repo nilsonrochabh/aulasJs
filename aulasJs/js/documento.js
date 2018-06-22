6e12139b5bbcf22433aabc796100948148ece250
nilsonrochabh <nilson.f.rocha@gmail.com>
2018-06-22 13:53:19 -0300

    Primeiro Comentário

---
+++ aulasJs/js/documento.js
@@ -0,0 +1,34 @@
+
+var nome = "Nilson";
+var idade= 35;
+var numero1;
+var numero2;
+var primeiroNome;
+
+
+numero1=10;
+numero2=30;
+
+var soma,multiplicacao,divisao,subitrair;
+
+soma= numero1+numero2;
+multiplica=numero1*numero2;
+divisao=numero1/numero2;
+subitrair=numero1-numero2;
+
+//imprimindo na tela
+alert(" Meu Nome é " +nome+" Minha idade é "+ idade);
+//alert(numero1);
+//alert(numero2);
+
+alert("A soma é "+soma+
+	   " A Multiplicação "+multiplica+
+	   " A Divisão é "+divisao+
+	   " A subitrair "+subitrair);
+
+
+
+
+
+primeiroNome = prompt("qual o seu nome?");
+alert(primeiroNome);
