030aa0ed4d3bf0f87d471f09f0d07ad23defba09
Nilson Rocha <nilson.f.rocha@gmail.com>
2018-07-13 12:01:28 -0300

    colocar na pasta js

--- aulasJs/documento.js
+++ aulasJs/documento.js
@@ -1,15 +1,24 @@
 
 function calcularIMC(){
-	var form = document.getElementById('form');
+	var form = document.getElementById('formulario');
 	var kilos = +form.kilos.value;
-	var metros = +form.metros.value;
-	var centrimetros = +form.centrimetros.value;
+	var metros = +form.metro.value;
+	
+	var centimetros = +form.centimetros.value;
 
 				//calculando os centimentros da pessoa
-	var altura = (metros * 100 + centrimetros)/100;
+	var altura = (metros * 100 + centimetros)/100;
 
 	var imc = kilos /(altura * altura);
 
 	form.imc.value=imc.toFixed(2); 
+
+
+	if(imc<18.5){
+
+		var element = document.getElementById("div1");
+   		 element.innerHTML=("<p> VC esta Muito Magro</p>");
+
+	}
 
 }
