da74066c2d9018fb5330d034e736789c5256752e
Nilson Rocha <nilson.f.rocha@gmail.com>
2018-07-31 15:56:43 -0300

    Subindo aula 31

--- js/documento.js
+++ js/documento.js
@@ -1,4 +1,3 @@
-
 function calcularIMC(){
 	var form = document.getElementById('formulario');
 	var kilos = +form.kilos.value;
@@ -6,7 +5,7 @@ function calcularIMC(){
 	
 	var centimetros = +form.centimetros.value;
 
-				//calculando os centimentros da pessoa
+	//calculando os centimentros da pessoa
 	var altura = (metros * 100 + centimetros)/100;
 
 	var imc = kilos /(altura * altura);
@@ -14,20 +13,24 @@ function calcularIMC(){
 	form.imc.value=imc.toFixed(2); 
 
 
-	if(imc<18.5){
-
-		var element = document.getElementById("div1");
-   		 element.innerHTML=("<p> VC esta Muito Magro</p>");
-
+	if(imc < 18.5){
+		var elemento = document.getElementById("div1");
+   		elemento.innerHTML="<p class='magro'> VC esta Muito Magro</p>";
+   	}
+	else if(imc>18.5 && imc < 25){
+		var elemento = document.getElementById("div1");
+   		 elemento.innerHTML=("<p class='ideal'> VC esta com o peso ideal</p>");
+	}
+	else if(imc>25 && imc < 30){
+		var elemento = document.getElementById("div1");
+   		 elemento.innerHTML=("<p class='levemente'> VC esta com o peso levemente gordo</p>");
+	}
+	else if(imc>30 && imc < 40){
+		var elemento = document.getElementById("div1");
+   		 elemento.innerHTML=("<p id='obeso1'> VC esta obesidade 1</p>");
+	}else if(imc> 40){
+		var elemento = document.getElementById("div1");
+   		 elemento.innerHTML=("<p id='obeso'> VC esta com obediade Morbida</p>");
 	}
 
-}
-
-
-function ligar() {
-	var lamp = document.getElementById("lamp");
-        if( lamp.src=="img/lapon.jpeg" )
-              lamp.src='img/lapoff.png';
-        else
-              lamp.src='img/lapon.jpeg';
 }
--- js/script.js
+++ js/script.js
@@ -1,91 +1,158 @@
 
+// var nome = "Nilson";
+// var idade= 35;
+// var numero1;
+// var numero2;
+// var primeiroNome;
+
+
+// numero1=10;
+// numero2=30;
+
+// var soma,multiplicacao,divisao,subitrair;
+
+// soma= numero1+numero2;
+// multiplica=numero1*numero2;
+// divisao=numero1/numero2;
+// subitrair=numero1-numero2;
+
+// //imprimindo na tela
+// alert(" Meu Nome é " +nome+" Minha idade é "+ idade);
+// //alert(numero1);
+// //alert(numero2);
+
+// alert("A soma é "+soma+
+// 	   " A Multiplicação "+multiplica+
+// 	   " A Divisão é "+divisao+
+// 	   " A subitrair "+subitrair);
+
+
+
+
+
+// primeiroNome = prompt("qual o seu nome?");
+// alert(primeiroNome);
+
+/*
+function mudaCor1(){
+	var xbolinha = document.getElementById('azul');
+	xbolinha.style.background="green";
+}
+
+function voltarCor1(){
+	var xbolinha=document.getElementById('azul');
+	xbolinha.style.background="yellow";
+}*/	
 
 
 
 
 
 
-
-
-var a = prompt('digite seu nome');
-TesteMensagem(a);
-function TesteMensagem(nome){
-	var div = document.getElementById('div');
+// function desafio(){
+// 	var v1 = +document.getElementById('primeiro').value;
+// 	var v2= +document.getElementById('segundo').value;
+// 	var v3 = +document.getElementById('terceiro').value;
 	
-	div.innerHTML= " Mandando uma mensagem concatenando com o nome "+ nome;
+// 	var soma = v1+v2+v3;
+// 	res =96;
+// 	if(v1==v2){
+// 		alert('Os numeros tem que ser diferentes :)');
+// 		location.reload();
+// 	}else if(v2==v3){
+// 		alert('Os numeros tem que ser diferentes :)');
+// 		location.reload();
+// 	}
+// 	else if(v3==v1){
+// 		alert('Os numeros tem que ser diferentes :)');
+// 		location.reload();
 
+// 	}else if(soma == res){
+// 		alert("parabens vc passou no desafio :)");
+// 		location.reload();
+		
+// 	}else if(soma !=res){
+// 		alert("A soma dos elementos é difente do resultado \nTente novamente :(");
+// 		location.reload();
+// 	}
+
+// }
+
+
+
+
+
+
+
+
+
+// function mudaCor2(){
+// 	var cor1=document.getElementById('amarelo');
+// 	cor1.style.background="purple";
+
+// }
+
+// function voltarCor2(){
+// 	var cor1=document.getElementById('amarelo');
+// 	cor1.style.background="blue";
+// }
+
+
+
+
+// function mudaCor3(){
+// 	var cor1=document.getElementById('vermelho');
+// 	cor1.style.background="#38B8AE";
+// }
+
+// function voltarCor3(){
+// 	var cor1=document.getElementById('vermelho');
+// 	cor1.style.background="red";
+// }
+// function mudaCor4(){
+// 	var cor1=document.getElementById('preto');
+// 	cor1.style.background="pink";
+// }
+
+// function voltarCor4(){
+// 	var cor1=document.getElementById('preto');
+// 	cor1.style.background="black";
+// }
+
+
+// function mudarImagem(){
+// 	document.getElementById("img1").src="img/sol.jpg";
+// }
+
+function nuvem1(){
+	var nuvem1 = document.getElementById('nuvem1');
+	nuvem1.src='img/nuvemFundo.png';
+}
+function voltar1(){
+	var nuvem1 = document.getElementById('nuvem1');
+	nuvem1.src='img/nuvem.png';
+}
+function nuvem2(){
+	var nuvem1 = document.getElementById('nuvem2');
+	nuvem1.src='img/nuvemFundo2.png';
+}
+
+function voltar2(){
+	var nuvem1 = document.getElementById('nuvem2');
+	nuvem1.src='img/nuvem.png';
+}
+function nuvem3(){
+	var nuvem1 = document.getElementById('nuvem3');
+	nuvem1.src='img/nuvemFundo3.png';
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function voltar3(){
+	var nuvem1 = document.getElementById('nuvem3');
+	nuvem1.src='img/nuvem.png';
+}
+
+function randomica(){
+	var randomica = document.getElementById('inputValor');
+	randomica.innerHTML = Math.floor(Math.random() * 1000);
+}
